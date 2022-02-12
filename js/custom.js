9d1ddb2cae65af021248e39869ea108333664ca8
Amit <amitgajera101@gmail.com>
2022-02-12 23:13:53 +0530

    Home,about,ecosystem pages design

---
+++ js/custom.js
@@ -0,0 +1,17 @@
+jQuery(document).ready(function () {
+	
+
+	// ecosystem slider js
+	jQuery('.fragmenting-slider').slick({
+		slidesToShow:1,
+		slidesToScroll:1,
+		infinite:true,
+		dots:false,
+		arrows: true,
+		fade: false,
+		adaptiveHeight: true,
+		autoplay: false,
+        autoplaySpeed: 6000
+	});
+
+});
